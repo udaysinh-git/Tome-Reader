4fda4d51cdf119a20ec3e0a4c555d9e3da25d43c
Vikleo <vikleoboy@gmail.com>
2025-02-22 14:14:13 +0530

    added some structure for the frount end layout and added fonts and etc

---
+++ client/src/comp/ReadBook.tsx
@@ -0,0 +1,27 @@
+import axios from "axios";
+import React from "react";
+import "./css/readBook.scss";
+import { useEffect, useState, useRef } from "react";
+import { useParams } from "react-router-dom";
+import EpubViewer from "../EpubViewer.tsx";
+// import { WebView } from "react-webview";
+// import "C:/Users/Vikleo/Desktop/books/Eric-Jorgenson_The-Almanack-of-Naval-Ravikant_EBOOK_v103/OEBPS/css/style.css";
+
+export const ReadBook = () => {
+  const { id } = useParams();
+  const [url, seturl] = useState(null);
+  useEffect(() => {
+    let base = "http://localhost:3002/";
+    let m = async () => {
+      let d = await axios.get(base + "Read?id=" + id);
+
+      seturl(d.data.url);
+    };
+
+    m();
+  }, []);
+
+  if (url !== null) {
+    return <EpubViewer bkid={id} url={url} />;
+  }
+};
