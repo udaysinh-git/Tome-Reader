c19bf847a64372070c85d6a5f6a47bfaaf670d35
Vikleo <vikleoboy@gmail.com>
2025-02-22 17:45:21 +0530

    complete side bar and workarea layout compleat

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,24 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
 
-createRoot(document.getElementById('root')!).render(
+import "@ionic/react/css/core.css";
+
+import { MainPage } from "./Main/MainPage.tsx";
+
+import { IonApp, setupIonicReact } from "@ionic/react";
+
+import { BrowserRouter } from "react-router-dom";
+
+setupIonicReact({
+  mode: "ios",
+});
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </StrictMode>,
-)
+);
